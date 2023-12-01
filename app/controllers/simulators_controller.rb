412e0d463f78a3174e1f74b296d363f48d763276
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 <harissonmagalhaes@gmail.com>
2023-12-01 14:56:28 -0300

    Adiciona funções para ativar e desativar simuladores

--- app/controllers/simulators_controller.rb
+++ app/controllers/simulators_controller.rb
@@ -2,4 +2,12 @@ class SimulatorsController < ApplicationController
   def index
     @simulators = Simulator.all
   end
+
+  def activate
+    @simulator.update(disabled: false)
+  end
+
+  def deactivate
+    @simulator.update(disabled: true)
+  end
 end
