75c47181c694ebfed95b6b6337649513e92b1164
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 <harissonmagalhaes@gmail.com>
2023-12-01 15:03:56 -0300

    Adiciona rotas para ativar e desativar simuladores

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   get 'simulators/index'
   get 'hello/world'
-  resources :simulators
+  resources :simulators do
+    member do
+      post 'activate'
+      post 'deactivate'
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
