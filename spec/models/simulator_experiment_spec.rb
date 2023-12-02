4ad6878fdb20d6c07027c8aa725268af1131b81e
BrunoVarg <brunovargas7899@gmail.com>
2023-12-01 23:37:28 -0300

    feat: adding test in rspec

--- spec/models/simulator_experiment_spec.rb
+++ spec/models/simulator_experiment_spec.rb
@@ -19,4 +19,11 @@ RSpec.describe SimulatorExperiment, type: :model do
     simulator_experiment = SimulatorExperiment.new(simulator: simulator, experiment: nil)
     expect(simulator_experiment).to_not be_valid
   end
+
+  it "is valid to get a experiment relationed to simulator" do
+    simulator = Simulator.new(name: "Simulator1", disabled: false)
+    experiment = Experiment.new(name: "Experiment1", disabled: false)
+    simulator_experiment = SimulatorExperiment.new(simulator: simulator, experiment: experiment)
+    expect(simulator_experiment.simulator).to eql(simulator)
+  end
 end
