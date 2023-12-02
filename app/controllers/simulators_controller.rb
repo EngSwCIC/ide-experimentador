103e0c800e437a36f3b916b07c4494503d94e45e
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 <harissonmagalhaes@gmail.com>
2023-12-01 23:48:09 -0300

    atualizando a função ativa/desativa, sou rota e testes

--- app/controllers/simulators_controller.rb
+++ app/controllers/simulators_controller.rb
@@ -1,13 +1,21 @@
 class SimulatorsController < ApplicationController
+  before_action :set_simulator, only: [:toggle]
   def index
     @simulators = Simulator.all
   end
 
-  def activate
-    @simulator.update(disabled: false)
-  end
+  def toggle
+    if params[:state] == 'activate'
+      @simulator.update(disabled: true)
+    elsif params[:state] == 'deactivate'
+      @simulator.update(disabled: false)
+    end
 
-  def deactivate
-    @simulator.update(disabled: true)
+    redirect_to simulators_path
+  end
+  private
+
+  def set_simulator
+    @simulator = Simulator.find(params[:id])
   end
 end
