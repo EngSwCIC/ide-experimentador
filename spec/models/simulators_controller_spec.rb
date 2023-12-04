0ab1b81a3a4fc0de60cb9f3335e215177b24cef6
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 <harissonmagalhaes@gmail.com>
2023-12-03 21:27:23 -0300

    Atualizando teste toggle

--- spec/models/simulators_controller_spec.rb
+++ spec/models/simulators_controller_spec.rb
@@ -3,19 +3,18 @@ require 'rails_helper'
 
 RSpec.describe SimulatorsController, type: :controller do
   describe 'POST #toggle' do
-    let!(:simulator) { Simulator.create(name: "Simulator Name", disabled: true) }
+    let(:simulator) { Simulator.create(name: "Simulator Name", disabled: true) }
 
 
     it 'activates the simulator' do
-      post :toggle, params: { id: simulator.id, state: 'activate' }
+      put :toggle, params: { id: simulator.id, simulator: { disabled: true } }
       simulator.reload
       expect(simulator.disabled).to be(false)
       expect(response).to redirect_to(simulators_path)
     end
 
     it 'deactivates the simulator' do
-      post :toggle, params: { id: simulator.id, state: 'deactivate' }
-      simulator.reload
+      put :toggle, params: { id: simulator.id, simulator: { disabled: false } }
       expect(simulator.disabled).to be(true)
       expect(response).to redirect_to(simulators_path)
     end
