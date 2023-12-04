728861e5403f180c6ca276ba5240bf75fdf606dc
pedronevz <pedro.a.neves12@gmail.com>
2023-12-03 23:58:51 -0300

    final commit sprint 2

--- app/javascript/react/src/index.js
+++ app/javascript/react/src/index.js
@@ -1,5 +1,6 @@
 import { define } from 'remount'      
 import Hello from "./components/Hello"
 import Velocity from "./components/Velocity"
+import Graph from './components/Graph'
                                       
-define({ 'hello-component': Hello, 'velocity-component': Velocity })
+define({ 'hello-component': Hello, 'velocity-component': Velocity, 'graph-component': Graph })
