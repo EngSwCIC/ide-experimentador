cbbcabf71a47bebbae599e321c50b3ae1038aaa0
Pedro Farias <pfarias29@gmail.com>
2023-12-15 00:53:31 -0300

    test: testes de parar e executar experimentos

--- test/models/experiment_test.rb
+++ test/models/experiment_test.rb
@@ -1,7 +1,33 @@
 require "test_helper"
 
 class ExperimentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  describe "Stop experiment" do
+    it "stops an experiment" do
+      visit experiments_path
+      click_button "Stop"
+      expect(experiment.status).to eq("stopped")
+    end
+  end
+
+  describe "Start experiment" do
+    it "starts an experiment" do
+      visit experiments_path
+      click_button "Start"
+      expect(experiment.status).to eq("running")
+    end
+  end  
+  
+  test "should stop experiment" do
+    experiment = experiments(:one)
+    experiment.stop
+    assert_equal experiment.status, "stopped"
+  end
+
+  test "should start experiment" do
+    experiment = experiments(:one)
+    experiment.start
+    assert_equal experiment.status, "running"
+  end
+
 end
