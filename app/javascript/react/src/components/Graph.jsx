7f443bf48cad0c19f7a342fe934234813353c130
pedronevz <pedro.a.neves12@gmail.com>
2023-12-17 17:58:27 -0300

    adaptando gráfico de skills

--- app/javascript/react/src/components/Graph.jsx
+++ app/javascript/react/src/components/Graph.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react'
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { LineChart, Line, BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import styles from './Graph.module.css'
 
 function Graph(){                                   
@@ -27,117 +27,73 @@ const Component = ({ req }) => {
     reqArray = [];
   }
 
+  semRepLista = []
+  reqArray.forEach(function(key, index){
+    switch (index){
+    case 0:
+      if(key.message == 'Experiment started!'){
+        semRepLista.push({"time": 0.00, "message": "Start!"})
+      }
+      break;
+    
+    case reqArray.length - 1:
+      if(key.message == 'Experiment completed successfully with 130.37 seconds!'){
+        semRepLista.push({"time": reqArray[index - 1].time, "message": "Success!"}) 
+      }
+      break;
+
+    default:
+      if(key.time == reqArray[index+1].time && key.message == reqArray[index+1].message){
+        if(semRepLista[semRepLista.length-1].message != key.message){
+          semRepLista.push(key)
+        }
+      }
+
+      if(key.time == reqArray[index+1].time && key.message != reqArray[index+1].message){
+        if(key.message == 'Sending message to nurse' || key.message == 'Waiting the message get to nurse'){
+          if(semRepLista[semRepLista.length-1].message != 'Sending message to nurse'){
+            semRepLista.push({"time": key.time, "message":"Sending message to nurse"})
+          }
+        }
+      }
+
+      if(key.message.includes("Message sent to")){
+        if(semRepLista[semRepLista.length-1].message != key.message){
+          semRepLista.push({"time": key.time, "message":'Message sent!'})
+        }
+      }
+    }  
+  })
+
+  console.log(reqArray)
+  console.log(semRepLista)
   return (
     <div>
-      {reqArray.map((item, index) => (
-        <div key={index}>
-          {item.hasOwnProperty('time') && (
-            <p>Time: {item.time}</p>
-          )}
-          <p>Message: {item.message}</p>
-        </div>
-      ))}
-      
-      <GraphSkills req = {reqArray}/>
+      <LineChart
+          width={1300}
+          height={300}
+          data={semRepLista}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5
+          }}
+      >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="message"/>
+          <YAxis dataKey="time"/>
+          <Tooltip />
+          <Legend />
+          <Line
+            type="monotone"
+            dataKey="time"
+            stroke="#8884d8"
+            activeDot={{ r: 8 }}
+          />
+      </LineChart>
     </div>
   );
 };
  
-
-const GraphSkills = ({req}) => {
-  
-  const labelSkills = []
-  const dataSkills = []
-
-  try {
-      req.forEach(function(key){
-          if(!labelSkills.includes(key.message)){
-            labelSkills.push(key.message)
-            dataSkills[labelSkills.indexOf(key.message)] = 1
-      
-          } else{
-            dataSkills[labelSkills.indexOf(key.message)] += 1
-          } 
-      })
-    } catch (error) {
-      console.error('Erro ao fazer parse da string JSON:', error);
-    }
-
-  var lista = []
-  labelSkills.forEach(function(key){
-    lista.push({"label": key, "data": dataSkills[labelSkills.indexOf(key)]})
-  })
-
-  
-  return (
-    <div>
-      <BarChart
-        width={3000}
-        height={700}
-        data={lista}
-        margin={{
-          top: 50,
-          right: 30,
-          left: 20,
-          bottom: 5
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="label" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Bar dataKey="data" fill="#8884d8" />
-      </BarChart>
-    </div>
-  )
-}
-
-/* const Graph = () =>{
-  let [req, setReq] = useState("")
-  useEffect(() => {
-    fetch("/graph/skillsGet").then(async res=>{
-      const parse = await res.text()
-      return parse
-    }).then(res=>setReq(res))
-  }, [])
-
-  /* reqArray = []
-
-  const labelSkills = []
-  const dataSkills = []
-
-  if(req.length > 0){
-    reqArray = JSON.parse(req);
-    reqArray.forEach(function(key){
-        if(!labelSkills.includes(key.message)){
-          labelSkills.push(key.message)
-          dataSkills[labelSkills.indexOf(key.message)] = 1
-    
-        } else{
-          dataSkills[labelSkills.indexOf(key.message)] += 1
-        } 
-    })
-  }
-
-   const [userData, setUserData] = useState({
-    labels: labelSkills,
-    datasets: [
-      {
-        label: "# Times Used",
-        data: dataSkills,
-      },
-    ],
-  }); 
-
-  
-
-  return (
-    <div>
-      <Component req={req} className={styles.req}></Component>
-    </div>
-) 
-}
- */
-
 export default Graph 
