10de44ccfd86839d4cbea3c888c82001c2211324
Alexandre <alexandrejunqueira2002@gmail.com>
2023-12-18 18:28:33 -0300

    fix: sidebar

--- app/javascript/react/src/index.js
+++ app/javascript/react/src/index.js
@@ -3,4 +3,4 @@ import Hello from "./components/Hello"
 import Graph from "./components/Graph"
 import Sidebar from "./components/Sidebar"
 
-define({ 'hello-component': Hello, 'graph-component': Graph, 'sidebar-componente': Sidebar })
+define({ 'hello-component': Hello, 'graph-component': Graph, 'sidebar-component': Sidebar })
