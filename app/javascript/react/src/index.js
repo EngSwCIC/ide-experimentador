23b19eb422453b9aefb43f00b604f8e749f97de9
PedroLife01 <pedrosardenberg@hotmail.com>
2023-12-19 17:30:19 -0300

    fix: buttons

--- app/javascript/react/src/index.js
+++ app/javascript/react/src/index.js
@@ -2,6 +2,7 @@ import { define } from 'remount'
 import Hello from "./components/Hello"
 import Graph from "./components/Graph"
 import StartExperiment from "./components/StartExperiment.js";
+import StopButton from "./components/StopButton.js";
 
 define({ "hello-component": Hello, "graph-component": Graph });
-define({ "start-button": StartExperiment });
+define({ "start-button": StartExperiment, "stop-button": StopButton });
