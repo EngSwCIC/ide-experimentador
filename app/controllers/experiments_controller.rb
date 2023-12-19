4a05ae0f75ece7f2fc4bfbb9c1bafbc7c7377ddc
jean karia <jeanbuenokaria@gmail.com>
2023-12-19 16:34:57 -0300

    cleaning

--- app/controllers/experiments_controller.rb
+++ app/controllers/experiments_controller.rb
@@ -1,32 +1,5 @@
 class ExperimentsController < ApplicationController
-    
     def index
         @experiments = Experiment.all
-        
     end
-    def show
-        @experiment = Experiment.find(params[:id])
-    end
-
-    def new
-        @experiment = Experiment.new
-    end
-
-    def create
-        @experiment = Experiment.new(experiment_params)
-        if @experiment.save
-            redirect_to root_path
-        else
-            render 'new'
-        end
-    end
-
-    def experiment_params
-        params.require(:experiment).permit(:name)
-    end
-
-    def search
-        @experiments = Experiment.where("name LIKE ?", "%#{params['search']}%")
-    end
-    
 end
