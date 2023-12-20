c72c534078dcf32788cffa171a9b1c93046c23ad
Quenom117 <dmonteiro117@gmail.com>
2023-12-20 16:53:23 -0300

    visualizarEfiltrar traduzido para pt

--- features/step_definitions/visualizarEfiltrar.rb
+++ features/step_definitions/visualizarEfiltrar.rb
@@ -6,39 +6,39 @@ Dado('que estou logado como usuário do ide-experimentador') do
     pending 
   end
   
-  When('eu acessar a página de experimentos') do
+  Quando('eu acessar a página de experimentos') do
     pending 
   end
   
-  Then('eu devo ver os todos experimentos existentes') do
+  Então('eu devo ver os todos experimentos existentes') do
     pending 
   end
   
-  Then('eu devo ver o botão {string}') do |string|
+  Então('eu devo ver o botão {string}') do |string|
     pending 
   end
   
-  When('eu clicar no botão {string}') do |string|
+  Quando('eu clicar no botão {string}') do |string|
     pending 
   end
   
-  Then('eu devo ver o campo {string}') do |string|
+  Então('eu devo ver o campo {string}') do |string|
     pending 
   end
   
-  Then('o filtro {string}') do |string|
+  Então('o filtro {string}') do |string|
     pending 
   end
   
-  When('eu clicar em {string}') do |string|
+  Quando('eu clicar em {string}') do |string|
     pending 
   end
   
-  Then('eu devo ver apenas os experimentos com a tag {string}') do |string|
+  Então('eu devo ver apenas os experimentos com a tag {string}') do |string|
     pending 
   end
   
-  Then('ver todos os experimento com a tag {string}') do |string|
+  Então('ver todos os experimento com a tag {string}') do |string|
     pending 
   end
   
@@ -46,14 +46,14 @@ Dado('que estou logado como usuário do ide-experimentador') do
     pending 
   end
   
-  When('eu acessar a pagina de experimentos') do
+  Quando('eu acessar a pagina de experimentos') do
     pending 
   end
   
-  When('nenhum filtro estiver selecionado') do
+  Quando('nenhum filtro estiver selecionado') do
     pending 
   end
   
-  Then('eu devo ver nenhum experimento') do
+  Então('eu devo ver nenhum experimento') do
     pending 
   end
