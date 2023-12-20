72eec6aaae69368ccf40d817f9e45c49c270efb5
felipecore <felipercore@gmail.com>
2023-12-20 17:53:36 -0300

    feat: rcov and rdoc

--- app/controllers/experiments_controller.rb
+++ app/controllers/experiments_controller.rb
@@ -1,4 +1,6 @@
+#Essa classe representa a manipulação dos experimentos cadastrados
 class ExperimentsController < ApplicationController
+    #Retorna todos os experimentos cadastrados para a realização da filtragem no código da página
     def index
         @experiments = Experiment.all
     end
