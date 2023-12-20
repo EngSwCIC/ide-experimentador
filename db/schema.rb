206c9bbc94779cfe4b8dfccfeca974b5c940eb35
jean karia <jeanbuenokaria@gmail.com>
2023-12-20 10:32:36 -0300

    coverage

--- db/schema.rb
+++ db/schema.rb
@@ -11,71 +11,10 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2023_12_19_100159) do
-  create_table "classifications", force: :cascade do |t|
-    t.integer "trial_id"
-    t.integer "tag_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["tag_id"], name: "index_classifications_on_tag_id"
-    t.index ["trial_id"], name: "index_classifications_on_trial_id"
-  end
-
   create_table "experiments", force: :cascade do |t|
     t.string "name"
     t.boolean "disabled"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-  create_table "factors", force: :cascade do |t|
-    t.string "name"
-    t.string "value"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "tags", force: :cascade do |t|
-    t.string "name"
-    t.string "color"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "trial_executions", force: :cascade do |t|
-    t.string "status"
-    t.text "log"
-    t.integer "trial_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["trial_id"], name: "index_trial_executions_on_trial_id"
-  end
-
-  create_table "trial_factors", force: :cascade do |t|
-    t.integer "factor_id"
-    t.integer "trial_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["factor_id"], name: "index_trial_factors_on_factor_id"
-    t.index ["trial_id"], name: "index_trial_factors_on_trial_id"
-  end
-
-  create_table "trials", force: :cascade do |t|
-    t.string "name"
-    t.boolean "disabled"
-    t.boolean "deleted"
-    t.integer "runs"
-    t.integer "experiment_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["experiment_id"], name: "index_trials_on_experiment_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
 end
---
+++ spec/requests/classifications_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "Classifications", type: :request do
+  describe "GET /index" do
+    it "returns http success" do
+      get "/classifications/index"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
